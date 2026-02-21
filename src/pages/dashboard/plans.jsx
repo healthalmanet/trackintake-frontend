bff5c84e6f42131db9ac60f2880ecb955e372779
unknown <vaibhavmishra71930@gmail.com>
2026-02-21 15:15:22 +0530

    plan

--- src/pages/dashboard/plans.jsx
+++ src/pages/dashboard/plans.jsx
@@ -105,7 +105,7 @@ const PlansPage = () => {
                   ₹{plan.price}
                 </span>
                 {plan.price > 0 && (
-                  <span className="text-sm"> / month</span>
+                  <span className="text-sm"> / days</span>
                 )}
               </div>
 
