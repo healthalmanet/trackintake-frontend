df9add6fbe444dfdc954e3243aa5f5495f902f47
unknown <vaibhavmishra71930@gmail.com>
2026-04-02 15:38:06 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://trackeats.onrender.com',
+        target: 'https://trackintake-backend.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
